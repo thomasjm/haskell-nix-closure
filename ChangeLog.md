18c74e45df467f124095f969259b3021b961976f
Tom McLaughlin <tom@codedown.io>
2020-08-25 23:02:25 -0700

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for haskell-nix-closure
+
+## Unreleased changes
---
+++ default.nix
@@ -0,0 +1,25 @@
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? builtins.fetchGit {
+  url = "https://github.com/codedownio/nixpkgs.git";
+  rev = "a543b8c8f2a8435943b9796873b616a44d2198d7";
+  ref = "codedown-20.07.3";
+  }
+
+# haskell.nix provides some arguments to be passed to nixpkgs, including some
+# patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+# import nixpkgs with overlays
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+}: pkgs.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "haskell-nix-closure";
+    src = ./.;
+  };
+}
